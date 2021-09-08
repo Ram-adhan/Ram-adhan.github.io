e1cdf1d4c9409f8286715a2a20672f7ddfc148c8
Ram-adhan <usworder@gmail.com>
2021-09-08 22:50:53 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "64276543f367260477d007fe3c0a3807",
-"/": "64276543f367260477d007fe3c0a3807",
-"main.dart.js": "0a2a1dc9e7775f623ff9458d780002e1",
+"index.html": "4fd8658051c113ab027c7639ba86e0f7",
+"/": "4fd8658051c113ab027c7639ba86e0f7",
+"main.dart.js": "7b8a8682941254526a1781fa8fb90835",
 "manifest.json": "6f974eecb176d7129058234ed55d866c",
 "version.json": "64012143e29ac710176ca25f0829bb0d"
 };
